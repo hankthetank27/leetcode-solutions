a23605bbec95249de72c42be8cb4c2236d3c3e12
Hank Jackson <hjackson277@gmail.com>
2022-07-29 14:47:08 -0400

    connected components count

---
+++ solutions/connected-components-count.ts
@@ -0,0 +1,51 @@
+/*
+Write a function, connectedComponentsCount, that takes in the adjacency list of an undirected graph. 
+The function should return the number of connected components within the graph.
+
+connectedComponentsCount({
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2]
+}); // -> 2
+
+*/
+
+type Graph = Record<string, number[]>;
+
+// interface Graph {
+//   [x: string] : number[];
+// }
+
+const connectedComponentsCount = (graph: Graph): number => {
+  const visited: Set<string> = new Set();
+  let count = 0;
+  for (let node in graph) {
+    if (dfsTraversal(graph, node, visited)) count++;
+  }
+  return count;
+};
+
+const dfsTraversal = (graph: Graph, node: string, visited: Set<string>): boolean => {
+  if (visited.has(node)) return false;
+  visited.add(node);
+  for (let value of graph[node]){
+    dfsTraversal(graph, String(value), visited);
+  }
+  return true;
+};
+
+const graph = {
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2]
+}
+
+console.log(connectedComponentsCount(graph)) // 2
