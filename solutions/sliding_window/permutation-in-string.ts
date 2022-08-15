9823b1988944befa3f048103a008ad361e58f747
Hank Jackson <hjackson277@gmail.com>
2022-08-15 11:00:50 -0400

    fixed permuations in string solution;

--- solutions/sliding_window/permutation-in-string.ts
+++ solutions/sliding_window/permutation-in-string.ts
@@ -20,38 +20,74 @@ Input: s1 = "ab", s2 = "eidboaoo"
 Output: false
 */
 
-
-//O(s1.length * n);
+//O(n)
 function checkInclusion(s1: string, s2: string): boolean {
+  
+  if (s1.length > s2.length) return false;
   
   const targetWord: Record<string, number> = {};
   
   for (let char of s1) {
-   targetWord[char] = 1 + (targetWord[char] || 0); 
+    targetWord[char] = 1 + (targetWord[char] || 0); 
   };
+
+  let leftPtr = 0;
+  let reqLen = s1.length;
   
-  let currentWord: Record<string, number> = {};
-  let left = 0;
-  
-  for (let right = 0; right < s2.length; right++){
-    currentWord[s2[right]] = 1 + (currentWord[s2[right]] || 0);
+  for (let rightPtr = 0; rightPtr < s2.length; rightPtr++){
     
-    if (right - left >= s1.length) {
-      currentWord[s2[left]]--;
-      left++;
-    };  
+    const leftChar = s2[leftPtr];
+    const rightChar = s2[rightPtr];
     
-    if (match(targetWord, currentWord)) return true;
+    //handle right
+    if (targetWord[rightChar] > 0) reqLen--;
+    targetWord[rightChar]--;
+    
+    //handle left
+    if (rightPtr - leftPtr === s1.length) {
+      if (targetWord[leftChar] >= 0) reqLen++;
+      targetWord[leftChar]++;
+      leftPtr++;
+    };
+
+    if (reqLen === 0) return true;
     
   };
   return false;
 };
 
 
-function match (targetWord: Record<string, number>, currentWord: Record<string, number>) {
-  let res = true;
-  for (let char in targetWord) {
-    if (currentWord[char] !== targetWord[char]) res = false;
-  };
-  return res
-};
+//O(26 * n);
+// function checkInclusion(s1: string, s2: string): boolean {
+  
+//   const targetWord: Record<string, number> = {};
+  
+//   for (let char of s1) {
+//     targetWord[char] = 1 + (targetWord[char] || 0); 
+//   };
+  
+//   const currentWord: Record<string, number> = {};
+//   let left = 0;
+  
+//   for (let right = 0; right < s2.length; right++){
+//     currentWord[s2[right]] = 1 + (currentWord[s2[right]] || 0);
+    
+//     if (right - left >= s1.length) {
+//       currentWord[s2[left]]--;
+//       left++;
+//     };  
+    
+//     if (match(targetWord, currentWord)) return true;
+    
+//   };
+//   return false;
+// };
+
+
+// function match (targetWord: Record<string, number>, currentWord: Record<string, number>) {
+//   let res = true;
+//   for (let char in targetWord) {
+//     if (currentWord[char] !== targetWord[char]) res = false;
+//   };
+//   return res
+// };
