b870307e9f1d6c8c835d01bb64c5bc38048cd082
Hank Jackson <hjackson277@gmail.com>
2022-11-10 11:23:58 -0500

    endcode and decode string js

--- js-solutions/linked-list/add-two-numbers.js
+++ js-solutions/linked-list/add-two-numbers.js
@@ -43,10 +43,39 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
 
-  let dummy = new ListNode();
-  let current = dummy;
+var addTwoNumbers = function(l1, l2, rem = 0) {
+  if (!l1 && !l2) return rem ? new ListNode(rem) : null;
+  
+  const val = (l1?.val || 0) + (l2?.val || 0) + rem;
+  const node = new ListNode();
+  
+  if (val >= 10){
+      node.val = val - 10;
+      rem = 1;
+  } else {
+      node.val = val;
+      rem = 0;
+  }
+  
+  
+  if (l1 && l2){
+      node.next = addTwoNumbers(l1.next, l2.next, rem);
+  } else if (l1){
+      node.next = addTwoNumbers(l1.next, l2, rem);
+  } else if (l2){
+      node.next = addTwoNumbers(l1, l2.next, rem);
+  }
+  
+  return node;
+};
+
+
+// iterative
+var addTwoNumbers2 = function (l1, l2) {
+
+  let head = new ListNode();
+  let current = head;
   let rem = 0;
 
   while (l1 || l2 || rem) {
@@ -73,5 +102,5 @@ var addTwoNumbers = function (l1, l2) {
     current = current.next;
 
   };
-  return dummy.next;
+  return head.next;
 };
