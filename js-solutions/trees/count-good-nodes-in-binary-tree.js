7b758f88daf74f1586a9d32536d76b89dd0a3386
Hank Jackson <hjackson277@gmail.com>
2022-12-19 17:28:39 -0500

    asdf

--- js-solutions/trees/count-good-nodes-in-binary-tree.js
+++ js-solutions/trees/count-good-nodes-in-binary-tree.js
@@ -38,10 +38,12 @@
 
 
 //Definition for a binary tree node.
-function TreeNode(val, left, right) {
-    this.val = (val===undefined ? 0 : val)
-    this.left = (left===undefined ? null : left)
-    this.right = (right===undefined ? null : right)
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+  }
 }
 
 /**
